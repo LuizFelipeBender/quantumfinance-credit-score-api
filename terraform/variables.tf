fab1014e1ad06c1d8c62c00564a674a2b7d64bb9
Luiz Felipe Alves Bender <LuizBender@outlook.com.br>
2025-08-02 15:46:39 -0300

    Deploydocker

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
-
 variable "image_uri" {
-  description = "URI da imagem Docker no ECR"
+  description = "URI da imagem Docker no ECR para o Lambda"
   type        = string
+  default     = "664406282152.dkr.ecr.us-east-1.amazonaws.com/quantumfinance/quantum-api:latest"
 }
